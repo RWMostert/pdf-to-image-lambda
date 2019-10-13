17952f577b01c5ce6125f21f991df6359bbb5d5c
RWMostert <rayno@explore-ai.net>
2019-10-13 13:52:02 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,69 @@
+# PDF-to-Image
+Convert a PDF document in a source S3 bucket to a image, saving the image to a destination S3 bucket.
+
+Thanks to Lambda's concurrency, this approach is well-suited to variable bulk/batch higher-volume conversion workloads.
+
+Please double check you are in the AWS region you intend; this needs to be the **same region** as the bucket which will contain the Word documents you wish to convert.
+
+##S3 Trigger
+This function can respond to S3 ObjectCreated events. 
+
+To configure the trigger, on the Lambda function, in "Designer > Add triggers", click "S3". The "Configure triggers" dialog appears.
+Select a bucket (any time a pdf is added to this bucket, the function will run).
+
+Verify that "all object create events" is selected (or choose PUT POST or COPY).
+
+Click "Add" (bottom right), then "Save" (top right).
+
+## Environment Variables
+
+On the same screen in the Lambda Management Console for this function, scroll down to "Environment Variables":
+
+You will need to set the output S3 Bucket for this function.  Optionally, you could also set the **format** and the **dpi** for the output image.
+
+### Required:
+
+**DESTINATION_BUCKET**: the name of the S3 bucket to which the PDF will be saved (if blank, it should write to the input event bucket)
+
+### Optional:
+
+**DPI**: Dots per inch, can be seen as the relative resolution of the output image, higher is better but anything above 300 is usually not discernable to the naked eye. Keep in mind that this is directly related to the ouput images size when using file formats without compression (like PPM).
+
+**FORMAT**: File format of the output images.  Supported values are "ppm", "jpeg", "png" and "tiff".
+
+## Execution Role
+
+Ensure that your execution role has both "s3:GetObject" and "s3:PutObject" permissions on both the source and destination buckets.
+
+The easiest is to open the Lambda Function settings, scroll down to the **Execution Role** section, and click "View the
+ pdf2image-dev role" on the IAM console.  Confirm that the role's policies includes S3 permissions, e.g.:
+
+ ```
+        {
+            "Effect": "Allow",
+            "Action": [
+                "s3:GetObject",
+                "s3:PutObject"
+            ],
+            "Resource": "arn:aws:s3:::*"
+        }
+```
+
+Without GetObject permission on the triggering bucket and PutObject permission on the output bucket, you'll get Access Denied errors.
+
+## Confirm successful installation
+### S3 Trigger
+If you configured the S3 trigger, you can try it, by copying a PDF document into the S3 bucket you have set the trigger on.
+
+To verify it works, look for a PDF in your output bucket, or check the logs in cloudwatch
+
+### Sizing Notes
+If you observe "Process exited before completing request" errors, it might point to your lambda function not having sufficient access to sufficient resources, or having insufficient time-out period.
+##### Memory
+Experience suggests assigning *2048MB*.  
+This can be set under the "Memory (MB)" header in the "Basic settings" section of the Lambda function configuration tab.
+
+##### Timeout
+The time taken for the Lambda to run, will depend on the size of the PDF document being processed.  For maximum flexibility, allow a 15 minute timeout, although experience suggests that the function should hardly ever take longer than a few seconds to run. 
+
+This can be set under the "Timeout" header in the "Basic settings" section of the Lambda function configuration tab.
---
+++ app.py
@@ -0,0 +1,104 @@
+import boto3
+import logging
+import os
+
+from chalice import Chalice, Response
+from io import BytesIO
+from pdf2image import convert_from_bytes
+
+app = Chalice(app_name='pdf2image')
+
+
+DPI = 300
+if 'DPI' in os.environ:
+    try:
+        DPI = int(os.environ['DPI'])
+    except Exception as e:
+        logging.debug(f"Couldn't process DPI environment variable: {str(e)}.  Using the default: DPI=300")
+else:
+    logging.info(f"No DPI environment variable set.  Using the default: DPI=300")
+
+_SUPPORTED_IMAGE_EXTENSIONS = ["ppm", "jpeg", "png", "tiff"]
+FMT = "png"
+if 'FMT' in os.environ:
+    environ_fmt = str(os.environ['FMT'])
+    if environ_fmt in _SUPPORTED_IMAGE_EXTENSIONS:
+        FMT = environ_fmt
+    else:
+        logging.debug(f"Couldn't process FMT variable.  "
+                      f"Only the following formats are supported: {','.join(_SUPPORTED_IMAGE_EXTENSIONS)}.  "
+                      f"Using the default: FMI='png'")
+else:
+    logging.info(f"No FMT environment variable set.  Using the default: FMT='png'")
+
+DESTINATION_BUCKET = None
+if 'DESTINATION_BUCKET' in os.environ:
+    DESTINATION_BUCKET = str(os.environ['DESTINATION_BUCKET'])
+    logging.info(f"Setting the destination bucket: {DESTINATION_BUCKET}. "
+                 f"Be sure to set the S3 bucket trigger on the Lambda's configuration")
+else:
+    raise Exception(f"Couldn't process the DESTINATION_BUCKET environment variable. "
+                    f"The DESTINATION_BUCKET needs to be set to a valid S3 bucket to which the user has full access.")
+
+ORIGIN_BUCKET = ''
+if 'ORIGIN_BUCKET' in os.environ:
+    ORIGIN_BUCKET = str(os.environ['ORIGIN_BUCKET'])
+    logging.info(f"Setting the origin bucket: {ORIGIN_BUCKET}. "
+                 f"Be sure to set the S3 bucket trigger on the Lambda's configuration")
+else:
+    logging.info(f"Couldn't process the ORIGIN_BUCKET environment variable. "
+                 f"Be sure to set the S3 bucket trigger on the Lambda's configuration.")
+
+_SUPPORTED_FILE_EXTENSION = '.pdf'
+
+
+@app.on_s3_event(bucket=ORIGIN_BUCKET,
+                 events=['s3:ObjectCreated:*'])
+def pdf_to_image(event):
+    """Take a pdf fom an S3 bucket and convert it to a list of pillow images (one for each page of the pdf).
+    :param event: A Lambda event (referring to an S3 event object created event).
+    :return:
+    """
+    if not event.key.endswith(_SUPPORTED_FILE_EXTENSION):
+        raise Exception(f"Only .pdf files are supported by this module.")
+
+    logging.info(f"Fetching item (bucket: '{event.bucket}', key: '{event.key}') from S3.")
+
+    # Fetch the image bytes
+    s3 = boto3.resource('s3')
+    obj = s3.Object(event.bucket, event.key)
+    infile = obj.get()['Body'].read()
+    logging.info("Successfully retrieved S3 object.")
+
+    # Set poppler path
+    poppler_path = "/var/task/lib/poppler-utils-0.26/usr/bin"
+    images = convert_from_bytes(infile,
+                                dpi=DPI,
+                                fmt=FMT,
+                                poppler_path=poppler_path)
+    logging.info("Successfully converted pdf to image.")
+
+    for page_num, image in enumerate(images):
+
+        # Then save the image and name it: <name of the pdf>-page<page number>.jpeg
+        location = f"{event.key}_page_{str(page_num)}.jpeg"
+
+        logging.info(f"Saving page number {str(page_num)} to S3 at location: {DESTINATION_BUCKET}, {location}.")
+
+        # Load it into the buffer and save the boytjie to S3
+        buffer = BytesIO()
+        image.save(buffer, "JPEG")
+        buffer.seek(0)
+        s3.Object(
+            DESTINATION_BUCKET,
+            location
+        ).put(
+            Body=buffer,
+            Metadata={
+                'original_document_key': event.key,
+                'page_number': str(page_num),
+                'page_count': str(len(images))
+            }
+        )
+
+    return Response(f"PDF document ({event.key}) successfully converted to a series of images.")
